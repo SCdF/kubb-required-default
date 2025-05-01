c75f36963421a532074dadc3422883117c455a57
Stefan du Fresne <stefan.dufresne@ovo.com>
2025-05-01 09:24:33 +0100

    Notes

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,33 @@
 import { getThingsQueryParamsSchema as zodOnlySchema } from "./_generated/zod-only/getThingsSchema.ts";
 import { getThingsQueryParamsSchema as zodTypedSchema } from "./_generated/zod-typed/getThingsSchema.ts";
 
-// because my open api schema defines a default for both query params
-// - neither of them should be number | undefined
-// - the entire type should not be able to be undefined
+/*
+  THE ISSUE(s)
+
+  I have two query parameters, both of them are optional with a default:
+
+  parameters:
+  - in: query
+    name: limit
+    description: Maximum number of things to return
+    schema:
+      type: integer
+      minimum: 1
+      maximum: 100
+      default: 100
+  - in: query
+    name: skip
+    description: Number of things to skip
+    schema:
+      type: integer
+      minimum: 0
+      default: 0
+
+I believe both Zod and Typescript plugins are parsing this incorrectly.
+
+Because my open api schema defines a default for both query params, this should
+result in a type like so:
+*/
 type ExpectedType = {
   limit: number;
   skip: number;
@@ -13,29 +37,42 @@ const iExpectThisToWork = (params: ExpectedType) => {
   console.log(params.skip);
 };
 
-type ZodOnlyActualType =
+/*
+  However we have two issues.
+
+  The first is that while sod has correctly detected that limit and skip are
+  required because they default (https://github.com/kubb-labs/kubb/issues/1290),
+  it still thinks the entire type is optional.
+*/
+type ZodOnlyActualDuckType =
   | {
       limit: number;
       skip: number;
     }
   | undefined;
-const zodOnlyActualType = (params: ZodOnlyActualType) => {
+const zodOnlyActualDuckType = (params: ZodOnlyActualDuckType) => {
   console.log(params);
 };
 
 console.log("Zod Only Schema");
 const thingQuery = zodOnlySchema.parse({});
-// iExpectThisToWork(thingQuery);
-zodOnlyActualType(thingQuery);
+iExpectThisToWork(thingQuery); // this should compile!!
+zodOnlyActualDuckType(thingQuery);
 
-type ZodTypedActualType = {
+/*
+  The second issue is that the Typescript plugin has incorrectly detected that
+  both limit and skip are optional, even though they have defaults.
+
+  Though interestingly it doesn't think that the entire type can be optional.
+*/
+type ZodTypedActualDuckType = {
   limit?: number;
   skip?: number;
 };
-const zodTypedActualType = (params: ZodTypedActualType) => {
+const zodTypedActualDuckType = (params: ZodTypedActualDuckType) => {
   console.log(params);
 };
 console.log("Zod Typed Schema");
 const thingTypedQuery = zodTypedSchema.parse({});
-// iExpectThisToWork(thingTypedQuery);
-zodTypedActualType(thingTypedQuery);
+iExpectThisToWork(thingTypedQuery); // this should compile!!
+zodTypedActualDuckType(thingTypedQuery);
